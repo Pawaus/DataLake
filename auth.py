b258bce621d03091570a0b13d25edd1ccd51d5a3
Pawaus <pawa.usov2013@gmail.com>
2022-04-01 14:19:12 -0700

    Add flash messages on login

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -10,6 +10,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
@@ -17,14 +19,10 @@ def login():
         passwd = request.form.get('inputPassword')
         user = User.query.filter_by(email=email).first()
         if not user or not check_password_hash(user.password, passwd):
+            flash("Email and/or password is incorrect")
             return redirect(url_for('auth.login'))
         login_user(user)
         return redirect(url_for('main.index'))
-
-
-@auth.route('/login_btn')
-def login_btn():
-    return redirect(url_for('main.index'))
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -37,6 +35,7 @@ def register():
         login = request.form.get('inputLogin')
         user = User.query.filter_by(email=email).first()
         if user:
+            flash('Email is already exist')
             return redirect(url_for('auth.register'))
         new_user = User(email=email, name=login, password=generate_password_hash(passwd, method='sha256'))
         db.session.add(new_user)
@@ -45,6 +44,7 @@ def register():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
